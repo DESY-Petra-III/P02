b91e4db0d4a4b1e94a45ef276879188be993da7b
Konstantin Glazyrin <lorcat@gmail.com>
2013-10-08 09:33:37 +0200

    resync of Online GP Ruby module

--- P02.2/production/beamline_control/modules/gui_module_ruby.py
+++ P02.2/production/beamline_control/modules/gui_module_ruby.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+# add modules to the list
+sys.path.append("/home/p02user/scripts/")
+sys.path.append("/home/p02user/scripts/modules")
 
 from  PyTango import *
 import pylab
