1e5fe9b3ceacbd87a9dd9bb85303950a744f6182
lorcat <lorcat@gmail.com>
2013-11-05 16:00:30 +0100

    update offline Raman stack

--- P02 Offline Raman/production/p3cntr/environment.py
+++
@@ -1,8 +0,0 @@
-# Lists all environment variables which should be set during startup
-
-Default_Timer = None
-Default_Measurement_Group = None
-
-Default_Scribe = None
-Default_Scribe_HDF5_mapping_file = None
-
